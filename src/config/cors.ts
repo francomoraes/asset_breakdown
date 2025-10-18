c2cb2282b68e8bc0fb9288be2e1b43c508f3f3e0
Franco Moraes <francoapmoraes@gmail.com>
2025-10-18 00:06:37 -0300

    feat: add localhost:8080 to CORS allowed origins

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -7,6 +7,7 @@ export const corsOptions: cors.CorsOptions = {
         "http://localhost:3000",
         "http://localhost:3001",
         "http://localhost:5173",
+        "http://localhost:8080",
       ]
     : config.frontendUrl === "*"
     ? true
